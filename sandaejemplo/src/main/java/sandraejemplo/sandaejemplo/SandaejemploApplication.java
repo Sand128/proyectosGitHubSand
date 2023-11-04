6aa65f07404ed5cb851c2455f5f24ef8ae140537
Sand128 <m.sandy.av@gmail.com>
2023-11-03 22:08:09 -0600

    mi primer spring

---
+++ sandaejemplo/src/main/java/sandraejemplo/sandaejemplo/SandaejemploApplication.java
@@ -0,0 +1,13 @@
+package sandraejemplo.sandaejemplo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SandaejemploApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SandaejemploApplication.class, args);
+	}
+
+}
---
+++ sandaejemplo/src/test/java/sandraejemplo/sandaejemplo/SandaejemploApplicationTests.java
@@ -0,0 +1,13 @@
+package sandraejemplo.sandaejemplo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SandaejemploApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
