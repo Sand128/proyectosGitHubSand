75fb93d9bcc95b13e47c80ad3f0577d9eeb7bd8b
Sand128 <131736085+Sand128@users.noreply.github.com>
2023-12-06 16:28:01 -0600

    ahora si el bueno
    ya tiene cambios de sintaxis, operaciones  y de funciones

--- metodotrapecio.py
+++ metodotrapecio.py
@@ -1,38 +1,41 @@
+import math
 def funcion(a):
-    #c=float(pow(a,3)-6*(pow(a,2))+(11*a)-6)
-    #c=float(pow(a,2)+10*a)
-    #c=float(pow(a,2))
-    #c=float(pow(a,3)+5*a)
-    c=float(a)
+    c=(pow(a,3)+2)
     return c
 
-def sumatoria(pi,pf):
+def funcionIntegral(pi,pf):
+    a=(((pow(pf,4)/4)+2*pf))
+    b=(((pow(pi,4)/4)+2*pi))
+    return a-b
+
+def proecedimiento(pi,pf):
     Area=0
+    suma=0
     i=0
     Npartes=int(input("Ingresa el numero de partes; "))
     paso=float((pf-pi)/Npartes)#paso=base
+    print("(pf-pi)/n=paso\n",(pf-pi),"/",Npartes,"=",paso)
     a=funcion(pi)
     b=funcion(pf)
-    resta=pf-pi
-    print("paso ",paso)
-    print("pf-pi= ",resta)
-    print("puntos pi,pf ",a," y ",b)
-    for i in range(Npartes-1):
-        area=(pi+(i*paso))
-        Area=area+Area
-    resultado= paso*((a/2)+(b/2)+Area)
+    #print("paso ",paso)
+    print("puntos f(pi)=",a," f(pf)",b)
+    res=pow((pf-pi),2)+pow((b-a),2)#siguiendo la formula para calcular la distanca, res=operacion dentro de la raiz
+    distancia=(math.sqrt(res))#calcular la distancia sacando la raiz de res
+    print("distancia del pi al pf = ",distancia)
+    for i in range(Npartes):
+        area=(Area+(i*paso))
+        Area=area
+        print(Area)
+        suma=Area+suma
+    print(suma," la suma de xn")
+    resultado= paso*((a/2)+(b/2)+suma)
     return resultado
 
-#print("Metodo del trapecio:  x^3-6x^2+11x-6")
-#print("Metodo del trapecio:  x^2+10x")
-#print("Metodo del trapecio:  x^2")
-#print("Metodo del trapecio:  x^3+5x")
-print("Metodo del trapecio:  x")
 
-
+print("Metodo del trapecio:  x^3+2")
 pi=float(input("Ingresa el Punto Inicial; "))
 pf=float(input("Ingresa el Punto Final; "))
-
-resultado=sumatoria(pi,pf)
+print("Resultado de la integral x^3+2, del punto ",pi," al ",pf," = ",funcionIntegral(pi,pf))
+resultado=proecedimiento(pi,pf)
 print("Area bajo la curva es: ",resultado)
 
