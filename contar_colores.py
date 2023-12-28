3026cfb7c5ba2983db40d1b3ede216856975e7b2
Sand128 <m.sandy.av@gmail.com>
2023-12-28 01:36:04 -0600

    listo, terminado cumple con lo pedido al recivir y retornar un string

--- contar_colores.py
+++ contar_colores.py
@@ -1,20 +1,10 @@
 def color_frecuente (lista):
     nwcolor=[]
-    nwcolor2=[]
-    cont=0
-    print(len(lista))
+    n=len(lista)
     for colores in sorted(set(lista)):
-        nwcolor.append(colores)
-        nwcolor2.append(lista.count((nwcolor[cont])))
-        cont=cont+1
-    lista1=nwcolor+nwcolor2
-    print(nwcolor2)
-    return  ""+max(lista1)
-
-
-    
-#mensaje="hay {} elementos coincidentes".format((nwcolor2[0]))
-#print(lista.count("azul"))   contar el numero de repeticiones dentro de la lista
-    
+        a=(lista.count((colores)))
+        nwcolor.append([colores,a])
+    mensaje=nwcolor.pop()
+    return  mensaje   
 lista=['azul', 'rojo', 'verde', 'verde', 'verde', 'rojo', 'verde', 'verde', 'azul', 'amarillo', 'azul', 'azul', 'verde', 'verde', 'verde', 'amarillo', 'amarillo']
 print(color_frecuente(lista))
