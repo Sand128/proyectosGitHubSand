24ecb933821623efaf0991354c09b07763fd75f1
Sand128 <m.sandy.av@gmail.com>
2024-02-03 21:17:43 -0600

    actualiza

--- prom_desviEstandar.py
+++ prom_desviEstandar.py
@@ -1,5 +1,5 @@
 #import statistics as stats
-import math
+from math import sqrt
 import numpy as np
 def promedio_std(lista):
     nwlista=[]
@@ -7,13 +7,25 @@ def promedio_std(lista):
     sum=0
     for elem in lista:
         sum=elem+sum
+    #print(sum)
     x=sum/n#promedio listo y tambien U para la desviacion estandar
+    #print(x)
     for elem in lista:
-        nwlista.append(pow(2,(elem-x)))
-    print(np.sum(nwlista))  
+        nwlista.append((elem-x)*(elem-x))
+    #print(np.sum(nwlista))  
     a=(np.sum(nwlista))/n
-    y=a**0.5
+    #print(a)
+    y=sqrt(a)
     return(x,y)
-lista=[10,9,8,7.5,6,10,9,7.8]
+lista = [81, 14, 45, 72, 57, 15, 67, 80, 40, 46, 19, 76, 87, 33, 56]
 print(len(lista))
 print(promedio_std(lista))
+
+
+
+"""
+Con lista = [81, 14, 45, 72, 57, 15, 67, 80, 40, 46, 19, 76, 87, 33, 56]
+promedio_std(lista) debió entregar 52.533 y 23.916, pero entregó 52.533 y 40257.383 
+Vuelve a intentarlo!
+
+"""
