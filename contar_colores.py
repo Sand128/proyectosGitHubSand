c45e5d99b794139c4b6d895523f3282b967df5c2
Sand128 <m.sandy.av@gmail.com>
2024-02-03 21:28:24 -0600

    actualiza

--- contar_colores.py
+++ contar_colores.py
@@ -1,10 +1,17 @@
 def color_frecuente (lista):
     nwcolor=[]
-    n=len(lista)
     for colores in sorted(set(lista)):
         a=(lista.count((colores)))
-        nwcolor.append([colores,a])
-    mensaje=nwcolor.pop()
-    return  mensaje   
-lista=['azul', 'rojo', 'verde', 'verde', 'verde', 'rojo', 'verde', 'verde', 'azul', 'amarillo', 'azul', 'azul', 'verde', 'verde', 'verde', 'amarillo', 'amarillo']
+        nwcolor.append([a,colores])
+    mensaje=max(nwcolor)
+    return mensaje
+lista =['amarillo', 'amarillo', 'amarillo', 'amarillo', 'amarillo', 'rojo', 'azul', 'verde', 'verde', 'azul', 'azul', 'amarillo', 'azul', 'azul', 'azul', 'amarillo', 'azul', 'azul', 'azul', 'azul', 'amarillo', 'azul', 'amarillo', 'rojo', 'azul', 'azul', 'azul', 'amarillo', 'rojo']
 print(color_frecuente(lista))
+
+
+"""
+Con lista = ['amarillo', 'amarillo', 'amarillo', 'amarillo', 'amarillo', 'rojo', 'azul', 'verde', 'verde', 'azul', 'azul', 'amarillo', 'azul', 'azul', 'azul', 'amarillo', 'azul', 'azul', 'azul', 'azul', 'amarillo', 'azul', 'amarillo', 'rojo', 'azul', 'azul', 'azul', 'amarillo', 'rojo']
+color_frecuente(lista) debió entregar "azul" y 14, pero entregó "verde" y 2 
+Vuelve a intentarlo!
+
+"""
